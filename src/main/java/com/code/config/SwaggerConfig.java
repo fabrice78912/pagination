ba61a984804327bc4d2b98f8c37d8a669bc39c90
fabrice78912 <fabricenokam@gmail.com>
2023-12-19 08:54:16 -0500

    Add open API

---
+++ src/main/java/com/code/config/SwaggerConfig.java
@@ -0,0 +1,14 @@
+package com.code.config;
+
+
+import org.springframework.context.annotation.Configuration;
+
+//@OpenAPIDefinition
+@Configuration
+public class SwaggerConfig {
+
+   /* @Bean
+    public OpenAPI baseOpenAPI(){
+        return new OpenAPI().info(new Info().title("Spring Doc").version("1.0").description("Spring Doc"));
+    }*/
+}
